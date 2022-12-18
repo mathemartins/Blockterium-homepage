b8bf57bda83ea336adb61b5c59275b08c28fcf91
bube365 <emenikeaustine36@gmail.com>
2022-12-18 19:23:07 +0800

    fixed import error

--- src/routes/navigation/navLinks/resourceLink.jsx
+++ src/routes/navigation/navLinks/resourceLink.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Resources } from "../Mylinks";
+import { Resources } from "../MyLinks";
 import {
   MdOutlineKeyboardArrowDown,
   MdOutlineKeyboardArrowUp,
